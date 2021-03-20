e6e98e6a761766b5e1c54f856afe13339f6ea422
Edwin <edwin41306@gmail.com>
2021-03-21 00:55:07 +0800

    uiUpdate

---
+++ source/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".fancy").fancybox({
+        openSpeed:300,
+        closeSpeed:300,
+        openEffect:"Elastic",
+        closeEffect:"fade",
+        padding:8,
+        maxWidth	: 1600,
+        maxHeight	: 1200, 
+        width		: '80%',
+        height		: '90%',
+        fitToView	: true,
+        autoSize	: true,
+    });
+});
