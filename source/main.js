af81a1ea85afd92f2f391e492b5269d7d5ede104
Edwin <edwin41306@gmail.com>
2021-04-16 16:18:25 +0800

    parallax

--- source/main.js
+++ source/main.js
@@ -16,12 +16,12 @@ $(document).ready(function(){
 
 document.addEventListener("mousemove", parallax);
 function parallax(e){
-  this.querySelectorAll('.box').forEach(box =>{
-    const speed = box.getAttribute('data-speed')
+  this.querySelectorAll('.layer').forEach(layer =>{
+    const speed = layer.getAttribute('data-speed')
     
-    const x =(window.innerWidth - e.pageX* speed)/200
-    const y =(window.innerHeight - e.pageY* speed) /200
+    const x =(window.innerWidth - e.pageX* speed)/ 100
+    const y =(window.innerHeight - e.pageY* speed) / 100
     
-    box.style.transform = `translateX(${x}px) translateY(${y}px)`
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`
   })
 }
