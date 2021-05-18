9e9a2b02cbe4e777712c79dddeddbdc82fa9abdd
Edwin <edwin41306@gmail.com>
2021-05-18 12:52:17 +0800

    fancybox3

--- style/main.js
+++ style/main.js
@@ -1,19 +1,3 @@
-$(document).ready(function(){
-    $(".fancy").fancybox({
-        openSpeed:300,
-        closeSpeed:300,
-        openEffect:"Elastic",
-        closeEffect:"fade",
-        padding:8,
-        maxWidth	: 1600,
-        maxHeight	: 1200, 
-        width		: '80%',
-        height		: '90%',
-        fitToView	: true,
-        autoSize	: true,
-    });
-});
-
 document.addEventListener("mousemove", parallax);
 function parallax(e){
   this.querySelectorAll('.layer').forEach(layer =>{
